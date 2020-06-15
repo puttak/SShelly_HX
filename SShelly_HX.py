ea14bff9fc1472af92ae0364938cbd1effdbc0c2
poshmine <63823993+poshmine@users.noreply.github.com>
2020-06-14 22:49:59 -0400

    Update SShelly_HX.py

--- SShelly_HX.py
+++ SShelly_HX.py
@@ -4,7 +4,8 @@ Author(s): Ryan McGuire*    Lane Carasik^
 *Virginia CommonWealth University
 *FAST Research Group
 Equations for Shell-Side Reactor Heat Exchangers: 
-Python Script for calculation of Shell-Side heat transfer coefficient using correctional factors for Shell-and-Tube bundles based on models found in literature.
+Python Script for calculation of Shell-Side heat transfer coefficient using correctional factors 
+for Shell-and-Tube bundles based on models found in literature.
 '''
 
 '''
